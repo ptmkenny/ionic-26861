24360270d19547345368b6fff91719f97e206b63
Patrick Kenny <github@ptmkenny.com>
2023-03-03 09:09:22 +0900

    show ion-select does not have isOpen set

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,10 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButton, IonContent, IonHeader, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <IonPage>
       <IonHeader>
@@ -17,6 +19,18 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <IonButton onClick={() => {
+          console.log('you clicked the button');
+          setIsOpen(true);
+        }}>
+          This is a button.
+        </IonButton>
+        <IonAlert isOpen={isOpen} message="This is an alert." />
+        <IonSelect label="Fruit" name="fruit" placeholder='Select a fruit'>
+          <IonSelectOption value="apples" key="apples">Apples</IonSelectOption>
+          <IonSelectOption value="grapes" key="grapes">Grapes</IonSelectOption>
+          <IonSelectOption value="other" key="other">Other</IonSelectOption>
+        </IonSelect>
       </IonContent>
     </IonPage>
   );
