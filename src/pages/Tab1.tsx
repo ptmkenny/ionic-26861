c5ff86b9c366733f93acf0a8dab862e5c6ef7636
Patrick Kenny <github@ptmkenny.com>
2023-03-04 09:44:49 +0900

    example of helpertext issue

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
@@ -17,6 +17,41 @@ const Tab1: React.FC = () => {
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 1 page" />
+        <p>with ion-item</p>
+        <IonItem>
+          <IonInput
+            type="email"
+            autocomplete="email"
+            errorText={undefined}
+            fill="outline"
+            label="INPUT WITH HELPERTEXT"
+            labelPlacement="floating"
+            helperText='This is some helper text.'></IonInput>
+            </IonItem>
+        <IonItem>
+          <IonInput
+            label="INPUT NO HELPERTEXT"
+            fill="outline"
+            labelPlacement="floating"
+          />
+        </IonItem>
+        <IonItem lines="none">
+          <IonButton>Submit</IonButton>
+        </IonItem>
+        <p>without ion-item</p>
+        <IonInput
+          type="email"
+          autocomplete="email"
+          errorText={undefined}
+          fill="outline"
+          label="INPUT WITH HELPERTEXT"
+          labelPlacement="floating"
+          helperText='This is some helper text.'></IonInput>
+        <IonInput
+          label="INPUT NO HELPERTEXT"
+          fill="outline"
+          labelPlacement="floating"
+        />
       </IonContent>
     </IonPage>
   );
